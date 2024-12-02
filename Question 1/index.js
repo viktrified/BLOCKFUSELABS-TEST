46f38cfefb608bb5b20d5a0bebd4510ea94d9566
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-12-02 10:37:37 +0100

    third commit

--- Question 1/index.js
+++ Question 1/index.js
@@ -4,13 +4,13 @@ let input2 = ["dog", "racecar", "car"];
 const findLongestPrefix = (arr) => {
   for (i = 0; i < arr.length; i++) {
     for (j = i + 1; j < arr.length; j++) {
-      if (arr[i].substring(0, 1) === arr[j].substring(0, 1)) {
-        return arr[i].substring(0, 1);
+      if (arr[i].substring(0, 2) === arr[j].substring(0, 2)) {
+        return arr[i].substring(0, 2);
       } else {
         return "";
       }
     }
   }
 };
-
+// console.log(input1[0].substring(0,2))
 console.log(findLongestPrefix(input2));
